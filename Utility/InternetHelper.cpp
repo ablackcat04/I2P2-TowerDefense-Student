aed87d8db20d7bb20590b11a5a1f52c9fbbd3234
ablackcat04 <wuru87@gmail.com>
2024-06-13 18:00:30 +0800

    online-scoreboard: link the curl c internet library and write a simple test of copying a file from localhost You need to put a file named hello_server.txt under /I2P_project of your localhost's folder

---
+++ Utility/InternetHelper.cpp
@@ -0,0 +1,31 @@
+//
+// Created by wuru8 on 2024/6/13.
+//
+#include "Engine/LOG.hpp"
+#include "InternetHelper.hpp"
+#include <curl/curl.h>
+
+size_t InternetHelper::write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+    size_t written = fwrite(ptr, size, nmemb, stream);
+    return written;
+}
+
+bool InternetHelper::downloadFile(const std::string &url, const std::string &localPath) {
+    Engine::LOG(Engine::INFO) << "Try to download file from" << url << " to " << localPath;
+    CURL *curl;
+    FILE *fp;
+    CURLcode res;
+    curl = curl_easy_init();
+    if(curl) {
+        fp = fopen(localPath.c_str(), "wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        res = curl_easy_perform(curl);
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+        fclose(fp);
+        return res == CURLE_OK;
+    }
+    return false;
+}
---
+++ Utility/InternetHelper.hpp
@@ -0,0 +1,20 @@
+//
+// Created by wuru8 on 2024/6/13.
+//
+
+#ifndef INC_2024_I2P2_TOWERDEFENSE_INTERNETHELPER_HPP
+#define INC_2024_I2P2_TOWERDEFENSE_INTERNETHELPER_HPP
+
+
+#include <iostream>
+#include <fstream>
+
+namespace InternetHelper {
+
+    size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream);
+
+    bool downloadFile(const std::string &url, const std::string &localPath);
+};
+
+
+#endif //INC_2024_I2P2_TOWERDEFENSE_INTERNETHELPER_HPP
