8457f06fed61276463cfb312a8304d515c5d8893
yuwei <studentyuwei@gapp.nthu.edu.tw>
2024-06-14 23:03:43 +0800

    Merge remote-tracking branch 'origin/gameengine' into gameengine
    # Conflicts:
    #	Scene/rhymegame.cpp

--- Scene/RhythmGameScene.hpp
+++ Scene/RhythmGameScene.hpp
@@ -29,7 +29,7 @@ public:
     float size;    // 音符的大小
     float starttime;
     bool active;
-    Note(int startX,float start) : x(startX), y(0), size(200),starttime(start),active(false){}// 构造函数初始化音符的起始位置
+    Note(int startX,float start) : x(startX), y(0), size(390),starttime(start),active(false){}// 构造函数初始化音符的起始位置
     void update(Conductor conduc) ;// 更新方法，用于更新音符的位置
     void render();// 渲染方法，用于在屏幕上绘制音符
 };
