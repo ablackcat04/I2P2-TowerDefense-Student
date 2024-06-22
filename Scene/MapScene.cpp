ebcb0493713c2c5d7c5e1e88e47084b9c5b72448
Blo-yen <sf170621@gmail.com>
2024-06-22 13:44:25 +0800

    Map Scene&button

--- Scene/MapScene.cpp
+++ Scene/MapScene.cpp
@@ -27,24 +27,35 @@ void MapScene::Initialize() {
     AddNewControlObject(btn);
     AddNewObject(new Engine::Label("Back", "pirulen.ttf", 48, halfW, halfH * 3 / 2, 0, 0, 0, 255, 0.5, 0.5));
 
-    btn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW/2 - 200, halfH / 2 - 50, 100,
-                                  100);
+    btn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW/2 - 150, halfH / 2 - 50, 200,
+                                  200);
     btn->SetOnClickCallback(std::bind(&MapScene::PlayOnClick, this, 1));
     AddNewControlObject(btn);
-    AddNewObject(new Engine::Label("1", "pirulen.ttf", 48, halfW/2-150, halfH / 2, 0, 0, 0, 255, 0.5, 0.5));
+    AddNewObject(new Engine::Label("1", "pirulen.ttf", 96, halfW/2-50, halfH / 2+50, 0, 0, 0, 255, 0.5, 0.5));
 if(cnt>1) {
-    btn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW/2 , halfH / 2 -50, 100,
-                                  100);
+    btn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW/2+300 , halfH / 2 -50, 200,
+                                  200);
     btn->SetOnClickCallback(std::bind(&MapScene::PlayOnClick, this, 2));
     AddNewControlObject(btn);
-    AddNewObject(new Engine::Label("2", "pirulen.ttf", 48, halfW/2+50, halfH / 2 , 0, 0, 0, 255, 0.5, 0.5));
+    AddNewObject(new Engine::Label("2", "pirulen.ttf", 96, halfW/2+400, halfH / 2 +50, 0, 0, 0, 255, 0.5, 0.5));
 }
 else {
-        AddNewObject(new Engine::Image("map/lock.jpg", halfW/2+50, halfH/2, 100, 100, 0.5, 0.5));
+    AddNewObject(new Engine::Image("map/lock.jpg", halfW/2+400, halfH/2+50, 200, 200, 0.5, 0.5));
+}
+    if (cnt>0) {
+        btn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW/2+750 , halfH / 2 -50, 200,
+                                     200);
+        btn->SetOnClickCallback(std::bind(&MapScene::PlayOnClick, this, 3));
+        AddNewControlObject(btn);
+        AddNewObject(new Engine::Label("3", "pirulen.ttf", 96, halfW/2+850, halfH / 2+50 , 0, 0, 0, 255, 0.5, 0.5));
+    }
+    else {
+        AddNewObject(new Engine::Image("map/lock.jpg", halfW/2+800, halfH/2+50, 200, 200, 0.5, 0.5));
     }
     // Not safe if release resource while playing, however we only free while change scene, so it's fine.
     bgmInstance = AudioHelper::PlaySample("select.ogg", true, AudioHelper::BGMVolume);
 }
+
 
 void MapScene::Terminate() {
     AudioHelper::StopSample(bgmInstance);
@@ -63,7 +74,7 @@ void MapScene::PlayOnClick(int stage) {
     Engine::GameEngine::GetInstance().ChangeScene("play");
 }
 void MapScene::Pluscnt() {
-    if(cnt<=2)cnt++;
+    if(cnt<=3)cnt++;
 }
 // Created by Prox64 on 2024/6/11.
 //
